7fa9d8627ec239667dc35ee09de0b1eb6bae40ad
Junhong Park <podolsmith@gmail.com>
2017-03-13 22:19:15 +0900

    finish InputReader

--- src/ant/Solution.java
+++ src/ant/Solution.java
@@ -1,6 +1,9 @@
 package ant;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -10,12 +13,27 @@ public class Solution {
 	}
 	
 	static class InputReader {
-		BufferedReader reader;
-		StringTokenizer tokenizer;
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
 		
-		public InputReader() {
-			reader = new BufferedReader(reader);
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
 			tokenizer = null;
+		}
+		
+		public String next() {
+			while ( tokenizer == null || !tokenizer.hasMoreTokens() ) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		
+		public int nextInt() {
+			return Integer.parseInt(next());
 		}
 	}
 
