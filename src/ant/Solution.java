a9309078f3e6f6d355622921e38927eef194edf0
Junhong <Junhong@JunhongPark>
2017-03-13 22:03:32 +0900

    git settings

---
+++ src/ant/Solution.java
@@ -0,0 +1,22 @@
+package ant;
+
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+	}
+	
+	static class InputReader {
+		BufferedReader reader;
+		StringTokenizer tokenizer;
+		
+		public InputReader() {
+			reader = new BufferedReader(reader);
+			tokenizer = null;
+		}
+	}
+
+}
