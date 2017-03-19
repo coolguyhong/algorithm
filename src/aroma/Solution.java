971607a217f0e1c1fd24873adf4e8361d9a1a48e
Junhong Park <podolsmith@gmail.com>
2017-03-19 22:29:01 +0900

    로직 1차 구현
    - 미완성 findMaximumDuration 함수 생성....

---
+++ src/aroma/Solution.java
@@ -0,0 +1,85 @@
+package aroma;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+/*
+ 아로마 오일 향기 최대 지속시간 구하기
+ 각각의 아로마 오일은 유통기한과 지속시간이 있음
+ 현재 사용하는 아로마 오일을 다 써야 다른 아로마 오일 사용가능
+ 유통기한이 지난 아로마 오일은 사용하지 못함
+ 유통기한이 0일 경우 지금 당장 사용 가능
+ 최대 사용 지속 시간을 구하여라
+ */
+	
+	static int NUMBER_OF_AROMA;
+	static int[] DURATION = new int[1000];
+	static int[] TIME = new int[1000];
+	static int ANSWER;
+	
+	public static void main(String[] args) {
+		InputReader input = new InputReader(System.in);
+		
+		int testCase = input.nextInt();
+		
+		for (int i = 1; i <= testCase; i++) {
+			NUMBER_OF_AROMA = input.nextInt();
+			
+			for (int j = 0; j < NUMBER_OF_AROMA; j++) {
+				DURATION[j] = input.nextInt();
+				TIME[j] = input.nextInt();
+			}
+			
+			findMaximumDuration(0);
+			
+			System.out.println("#" + i + " " + ANSWER);
+		}
+	}
+	
+	private static void findMaximumDuration(int order) {
+		for (int i = 0; i < NUMBER_OF_AROMA; i++) {
+			if (i == order) {
+				continue;
+			}
+			if (DURATION[i] < 0) {
+				continue;
+			}
+			
+			DURATION[i] = DURATION[i] - TIME[order];
+			
+			if (order < NUMBER_OF_AROMA) {
+				findMaximumDuration(order+1);
+			}
+			
+		}
+	}
+
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+	
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
+		
+		public String next() {
+			while ( tokenizer == null || !tokenizer.hasMoreTokens() ) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+	}
+}
