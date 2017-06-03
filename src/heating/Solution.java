3f8215a1cb3be592027edcdca7f3bb12a6854cca
Junhong Park <podolsmith@gmail.com>
2017-06-03 12:58:05 +0900

    solution class 구현

--- src/heating/Solution.java
+++ src/heating/Solution.java
@@ -1,9 +1,41 @@
 package heating;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
 public class Solution {
 
 	public static void main(String[] args) {
-
+		InputReader input = new InputReader(System.in);
+	}
+	
+	static class InputReader {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+	
+		public InputReader(InputStream stream) {
+			reader = new BufferedReader(new InputStreamReader(stream), 32768);
+			tokenizer = null;
+		}
+		
+		public String next() {
+			while ( tokenizer == null || !tokenizer.hasMoreTokens() ) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
 	}
 
 }
