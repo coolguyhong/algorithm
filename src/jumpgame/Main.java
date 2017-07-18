74baad195a422ba5b2499488318619d07be7067c
Junhong Park <podolsmith@gmail.com>
2017-07-18 21:45:57 +0900

    apply memoization but not solve it yet

--- src/jumpgame/Main.java
+++ src/jumpgame/Main.java
@@ -9,49 +9,48 @@ import java.util.StringTokenizer;
 public class Main {
 	private static int n;
 	private static int board[][] = new int[100][100];
-	private static int cache[][] = new int[100][100];
+	private static boolean cache[][] = new boolean[100][100];
 
 	public static void main(String[] args) {
 		InputReader input = new InputReader(System.in);
-		
+		long start = System.currentTimeMillis();
+
 		final int testCase = input.nextInt();
 		for (int i = 1; i <= testCase; i++) {
 			n = input.nextInt();
 			// board 만들기
 			for (int j = 0; j < n; j++) {
 				for (int k = 0; k < n; k++) {
-					cache[j][k] = -1;
+					cache[j][k] = false;
 					board[j][k] = input.nextInt();
 				}
 			}
 			
-			if(jump(0, 0) == 1) {
+			if(jump(0, 0)) {
 				System.out.println("YES");
 			} else {
 				System.out.println("NO");
 			}
 		}
+		long elapsedTime = System.currentTimeMillis() - start;
+		System.out.println( "실행 시간 : " + elapsedTime +  " ms");
 	}
 
-	private static int jump(int x, int y) {
+	private static boolean jump(int y, int x) {
 		//board 밖으로 벗어난 경우
-		if (x >= n || y >= n) {
-			return 0;
+		if (y >= n || x >= n) {
+			return false;
 		}
 		
 		//board에 도착했을 경우
-		if (x == n-1 && y == n-1) {
-			return 1;
+		if (y == n-1 && x == n-1) {
+			return true;
 		}
-		
-		int cache1 = cache[x][y];
-		if (cache1 != -1) {
-			return cache1;
+		if (cache[y][x]) {
+			return cache[y][x];
 		}
-		
-		int jumpSize = board[x][y];
-		
-		return cache1 = (jump(x + jumpSize, y) + jump(x, y + jumpSize));
+		int jumpSize = board[y][x];
+		return cache[y][x] = (jump(y + jumpSize, x) || jump(y, x + jumpSize));
 	}
 	
 	static class InputReader {
