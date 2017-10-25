1839480070b4bfc777081154a3be995fe3754dd5
coolguyhong <jun64.park@samsung.com>
2017-10-25 15:24:57 +0900

    test case 3 깨짐

--- src/road/Main.java
+++ src/road/Main.java
@@ -1,17 +1,16 @@
-package load;
+package road;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	private static int[] VISITED = new int[1001];
+	private static int[] DISCOVERED = new int[1001];
 	private static int N;
 	private static int M;
+	private static int SEQUENCE = 0;
 	private static int[][] ROAD = new int[1001][1001];
 	private static List<Integer> ANSWER = new ArrayList<>();
 
@@ -28,32 +27,42 @@ public class Main {
 				ROAD[x][y] = j;
 			}
 
-			dfs(1, 0, 1);
-			System.out.print("#" + testCase + " " + ANSWER.size()+ " ");
-			for (Integer ans : ANSWER) {
-				System.out.print(ans + " ");
+			dfs(1, 0);
+
+			System.out.print("#" + i + " " + ANSWER.size() + " ");
+			if (!ANSWER.isEmpty()) {
+				Collections.sort(ANSWER);
+				for (Integer ans : ANSWER) {
+					System.out.print(ans + " ");
+				}
+				ANSWER.clear();
 			}
+			SEQUENCE = 0;
 			System.out.println();
-			ANSWER.clear();
 		}
 	}
 
-	private static void dfs(int h, int p, int d) {
-		VISITED[h] = d;
+	private static int dfs(int current, int par) {
+		DISCOVERED[current] = ++SEQUENCE;
+		int ret = DISCOVERED[current];
+
 		for (int i = 1; i <= N; ++i) {
-			if (ROAD[h][i] == p) {
+			if (i == par || ROAD[current][i] == 0) {
 				continue;
 			}
-			if (VISITED[i] != 0) {
-				dfs(i, h, d + 1);
-				if (VISITED[i] == d + 1) {
-					ANSWER.add(ROAD[h][i]);
-				}
-			}
-			VISITED[h] = Math.min(VISITED[h], VISITED[i]);
-		}
-	}
 
+			if (DISCOVERED[i] == 0) {
+				int df = dfs(i, current);
+				if (df > DISCOVERED[current]) {
+					ANSWER.add(ROAD[current][i]);
+				}
+				ret = Math.min(ret, df);
+			} else {
+				ret = Math.min(ret, DISCOVERED[i]);
+			}
+		}
+		return ret;
+	}
 
 	static class InputReader {
 		public BufferedReader reader;
