753b1f0528bb79c6a3e0004cf09b258f874f54b9
coolguyhong <podolsmith@naver.com>
2018-12-16 16:45:31 +0900

    package 정비

--- src/bfs/Solution.java
+++
@@ -1,7 +0,0 @@
-package bfs;
-
-public class Solution {
-    public static void main(String[] args) {
-
-    }
-}
--- src/withdrawal/Main.java
+++
@@ -1,9 +0,0 @@
-package withdrawal;
-
-public class Main {
-
-
-    public static void main(String[] args) {
-
-    }
-}
