e7182a127656570e84b5773fc068e5a3873888e9
coolguyhong <podolsmith@naver.com>
2019-10-24 21:07:55 +0900

    백준 알고리즘 복제로봇 유니언 파인드, bfs

--- src/graph/baekjoon/clone_robot/Main.java
+++ src/graph/baekjoon/clone_robot/Main.java
@@ -13,12 +13,12 @@ public class Main {
     private static StringTokenizer st;
 
     private static int N, M;
-    private static int[] parents;
+    private static int[] par;
     private static char[][] maze;
     private static Deque<Node> nodes;
     private static List<Edge> edges;
-    private static int[] dx = {0, 0, 1, -1};
-    private static int[] dy = {1, -1, 0, 0};
+    private static int[] dx = {1, -1, 0, 0};
+    private static int[] dy = {0, 0, 1, -1};
     private static int[][] node;
 
     // 백준 알고리즘
@@ -33,21 +33,21 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
 
         maze = new char[N+1][N+1];
+        node = new int[N+1][N+1];
         nodes = new ArrayDeque<>();
         for (int i = 1; i <= N; i++) {
             String str = br.readLine();
             for (int j = 1; j <= N; j++) {
                 char c = str.charAt(j-1);
-                maze[i][j] = c;
-                if (maze[i][j] == 'S') {
+                if (c == 'S') {
                     nodes.addFirst(new Node(i, j));
-                } else if (maze[i][j] == 'K') {
+                } else if (c == 'K') {
                     nodes.add(new Node(i, j));
                 }
+                maze[i][j] = c;
             }
         }
 
-        node = new int[N+1][N+1];
         int idx = 1;
         for (Node n : nodes) {
             node[n.x][n.y] = idx++;
@@ -60,23 +60,24 @@ public class Main {
 
         Collections.sort(edges);
 
-        parents = new int[M+2];
+        par = new int[M+2];
         for (int i = 1; i <= M+1; i++) {
-            parents[i] = i;
+            par[i] = i;
         }
 
         int cnt = 0;
         int ans = 0;
-        for (Edge edge : edges) {
-            int ps = find(edge.s);
-            int pe = find(edge.e);
+        for (Edge e : edges) {
+            int ps = find(e.s);
+            int pe = find(e.e);
+
             if (ps == pe) {
                 continue;
             }
 
-            parents[pe] = ps;
+            par[pe] = ps;
             cnt++;
-            ans += edge.c;
+            ans += e.c;
 
             if (cnt == M) {
                 break;
@@ -88,46 +89,44 @@ public class Main {
         } else {
             bw.write(ans + "\n");
         }
+
         bw.close();
     }
 
-    private static int find(int s) {
-        if (parents[s] == s) {
-            return s;
+    private static int find(int a) {
+        if (par[a] == a) {
+            return a;
         } else {
-            return parents[s] = find(parents[s]);
+            return par[a] = find(par[a]);
         }
     }
 
-    private static void bfs(int x, int y) {
+    private static void bfs(int sx, int sy) {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x, y, 0});
+        q.add(new int[]{sx, sy, 0});
         int[][] visited = new int[N+1][N+1];
-        visited[x][y]++;
+        visited[sx][sy]++;
 
         int[] n;
         while (!q.isEmpty()) {
             n = q.poll();
-            int cx = n[0];
-            int cy = n[1];
-            int d = n[2];
 
             for (int i = 0; i < 4; i++) {
-                int tx = cx + dx[i];
-                int ty = cy + dy[i];
+                int tx = n[0] + dx[i];
+                int ty = n[1] + dy[i];
 
                 if (tx < 1 || tx > N || ty < 1 || ty > N) {
                     continue;
                 }
 
-                if (visited[tx][ty] != 0 || maze[tx][ty] == '1') {
+                if (visited[tx][ty] !=0 || maze[tx][ty] == '1') {
                     continue;
                 }
 
                 visited[tx][ty]++;
-                q.add(new int[]{tx, ty, d+1});
+                q.add(new int[]{tx, ty, n[2] + 1});
                 if (maze[tx][ty] == 'S' || maze[tx][ty] == 'K') {
-                    edges.add(new Edge(node[x][y], node[tx][ty], d+1));
+                    edges.add(new Edge(node[sx][sy], node[tx][ty], n[2] + 1));
                 }
             }
         }
@@ -136,7 +135,6 @@ public class Main {
 
 class Node {
     int x, y;
-
     Node(int x, int y) {
         this.x = x;
         this.y = y;
