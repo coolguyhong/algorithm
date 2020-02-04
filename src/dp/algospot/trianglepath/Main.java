3edbde5229542038862d2d217e7dd68564c4b4f1
coolguyhong <podolsmith@naver.com>
2020-02-04 21:06:55 +0900

    알고스팟 triangle path

--- src/dp/algospot/trianglepath/Main.java
+++ src/dp/algospot/trianglepath/Main.java
@@ -1,79 +1,56 @@
 package dp.algospot.trianglepath;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
+// 알고스팟
+// https://algospot.com/judge/problem/read/TRIANGLEPATH
+// 알고스팟(dp) 최적화, triangle path
 public class Main {
-	private static int sizeOfTriangle;
-	private static int[][] Triangle = new int[100][100];
-	private static int[][] cache = new int[100][100];
 
-	public static void main(String[] args) {
-		InputReader input = new InputReader(System.in);
-		int testCase = input.nextInt();
-		while (testCase > 0) {
-			sizeOfTriangle = input.nextInt();
-			for (int i = 0; i < sizeOfTriangle; i++) {
-				for (int j = 0; j <= i; j++) {
-					Triangle[i][j] = input.nextInt();
-					cache[i][j] = -1;
+	private static BufferedReader br;
+	private static BufferedWriter bw;
+	private static StringTokenizer st;
+
+	private static int n;
+	private static int[][] triangle, D;
+
+	public static void main(String[] args) throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int testCase = Integer.parseInt(br.readLine());
+		while (testCase-- > 0) {
+			n = Integer.parseInt(br.readLine());
+			triangle = new int[n+1][n+1];
+			D = new int[n+1][n+1];
+			for (int i = 1; i <= n; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 1; j <= i; j++) {
+					triangle[i][j] = Integer.parseInt(st.nextToken());
+					D[i][j] = -1;
 				}
 			}
 
-			List<Integer> sd = new ArrayList<>();
-			sd.add(11);
-
-			for (int i = 0; i < sd.size(); i++) {
-				System.out.println(sd.get(i).intValue());
-			}
-			
-			System.out.println(findMaxSum(0, 0));
-			testCase--;
+			bw.write(path(1, 1) + "\n");
 		}
+		bw.close();
 	}
 	
-	private static int findMaxSum(int y, int x) {
+	private static int path(int x, int y) {
 		//삼각형 제일 하단으로 내려온 경우
-		if (y == sizeOfTriangle - 1) {
-			return Triangle[y][x];
+		if (x == n) {
+			D[x][y] = triangle[x][y];
+			return D[x][y];
 		}
-		
-		//초기값일 경우
-		if (cache[y][x] == -1) {
-			cache[y][x] = Triangle[y][x];
-		} else {
-			return cache[y][x];
-		}
-		return cache[y][x] = Math.max(findMaxSum(y+1, x), findMaxSum(y+1, x+1)) + Triangle[y][x];
-	}
 
-	static class InputReader {
-		public BufferedReader reader;
-		public StringTokenizer tokenizer;
-		
-		public InputReader(InputStream stream) {
-			reader = new BufferedReader(new InputStreamReader(stream), 32768);
-			tokenizer = null;
+		if (D[x][y] != -1) {
+			return D[x][y];
 		}
 		
-		public String next() {
-			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-				try {
-					tokenizer = new StringTokenizer(reader.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return tokenizer.nextToken();
-		}
-		
-		public int nextInt() {
-			return Integer.parseInt(next());
-		}
+		return D[x][y] = Math.max(path(x+1, y), path(x+1, y+1)) + triangle[x][y];
 	}
 }
